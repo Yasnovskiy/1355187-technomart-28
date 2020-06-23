17ad54bc2a6b3f2205f052acc77673d1c6388857
Yasnovskiy <64742074+Yasnovskiy@users.noreply.github.com>
2020-06-23 16:00:06 +0500

    Выкатил

--- js/document.js
+++ js/document.js
@@ -9,97 +9,92 @@ let arrange = document.querySelector('.modal-basket-list-button-issue');
 let resume = document.querySelector('.modal-basket-list-button-resume');
 
 let modalCommunicationForm =  document.querySelector('.modal-communication-form');
-let communicationName =  document.querySelector( '.communication-name');
-let communicationEmail =  document.querySelector( '.communication-email');
-let communicationLetter =  document.querySelector( '.communication-letter');
-let modalCommunication =  document.querySelector( '.modal-communication');
+let communicationName =  document.querySelector('.communication-name');
+let communicationEmail =  document.querySelector('.communication-email');
+let communicationLetter =  document.querySelector('.communication-letter');
+let modalCommunication =  document.querySelector('.modal-communication');
 
 let name = document.querySelector('[name=name]');
 let email = document.querySelector('[name=email]');
 
 
 if (link) {
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    map.classList.add('modal-show');
-    console.log('Отлично!');
-});
+  link.addEventListener ("click", function (evt) {
+    evt.preventDefault ();
+    map.classList.add ('modal-show');
+    console.log ('Отлично!');
+  });
 };
 
 if (close) {
-  close.forEach(function (item){
-  item.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    item.parentElement.classList.remove('modal-show');
-    console.log('Кнопка закрыть');
-});
-});
+    close.forEach(function (item) {
+    item.addEventListener ("click", function (evt) {
+      evt.preventDefault ();
+      item.parentElement.classList.remove ('modal-show');
+      console.log ('Кнопка закрыть');
+    });
+  });
 };
 
 if(buy) {
-  buy.forEach(function (item){
-    item.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      modalBasketCatalog.classList.add('modal-show');
+  buy.forEach(function (item) {
+    item.addEventListener ('click', function (evt) {
+      evt.preventDefault ();
+      modalBasketCatalog.classList.add ('modal-show');
     });
   });
 };
 
 if (write) {
-  write.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    communication.classList.add('modal-show');
-    name.focus();
-    console.log('умный!');
-});
+  write.addEventListener ("click", function (evt) {
+    evt.preventDefault ();
+    communication.classList.add ('modal-show');
+    name.focus ();
+    console.log ('умный!');
+  });
 };
-
-
 
 if(modalCommunicationForm){
     modalCommunicationForm.addEventListener ('submit', function (evt) {
         if (!communicationName.value || !communicationEmail.value || !communicationLetter.value) {
-                evt.preventDefault ();
-                modalCommunication.classList.remove('modal-error');
-                modalCommunication.offsetWidth = modalCommunication.offsetWidth;
-                modalCommunication.classList.add('modal-error');
+          evt.preventDefault ();
+          modalCommunication.classList.remove ('modal-error');
+          modalCommunication.offsetWidth = modalCommunication.offsetWidth;
+          modalCommunication.classList.add ('modal-error');
         }
     });
 };
 
-
 if (arrange) {
-  arrange.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalBasketCatalog.classList.remove('modal-show');
-    console.log('Ах');
-});
+  arrange.addEventListener ("click", function (evt) {
+    evt.preventDefault ();
+    modalBasketCatalog.classList.remove ('modal-show');
+    console.log ('Ах');
+  });
 };
 
 if (resume) {
-  resume.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalBasketCatalog.classList.remove('modal-show');
-    console.log('Ух!');
-});
+  resume.addEventListener ("click", function (evt) {
+    evt.preventDefault ();
+    modalBasketCatalog.classList.remove ('modal-show');
+    console.log ('Ух!');
+  });
 };
 
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27){
+window.addEventListener ('keydown', function (evt) {
+  if (evt.keyCode === 27) {
     if (communication.classList.contains ('modal-show')) {
-      evt.preventDefault();
-      communication.classList.remove('modal-show');
-  }
-}
+      evt.preventDefault ();
+      communication.classList.remove ('modal-show');
+    }
+  };
 });
 
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27){
+window.addEventListener ('keydown', function (evt) {
+  if (evt.keyCode === 27) {
     if (map .classList.contains ('modal-show')) {
-      evt.preventDefault();
-      map .classList.remove('modal-show');
-  }
-}
+      evt.preventDefault ();
+      map .classList.remove ('modal-show');
+    }
+  };
 });
